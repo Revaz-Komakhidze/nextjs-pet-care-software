b642408730c3193d179374b909406cb4531125ca
Rezo Komakhidze <rezokomakhidze1@gmail.com>
2024-03-07 13:50:29 +0400

    modified bg color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} text-sm text-slate-900 bg-white`}>
+      <body
+        className={`${inter.className} text-sm text-slate-900 bg-[#E5E8EC] min-h-screen`}
+      >
         {children}
       </body>
     </html>
